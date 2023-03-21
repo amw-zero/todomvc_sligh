e5b6e9c96595a7c9c098cf72a901cb3c1282ffec
Alex Weisberger <alex@relaypayments.com>
2023-03-20 23:07:42 -0400

    Prisma todo data.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { GetStaticProps } from 'next'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import prisma from '../../lib/prisma'
+import { Todo } from '@prisma/client'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export const getStaticProps: GetStaticProps = async () => {
+  const todos = await prisma.todo.findMany();
+
+  return {
+    props: { todos },
+    revalidate: 10,
+  };
+};
+
+type Props = {
+  todos: Todo[],
+}
+
+export default function Home({ todos }: Props) {
   return (
     <>
       <Head>
@@ -15,6 +31,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <p>Todos</p>
+        {todos.map(t => t.name)}
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
